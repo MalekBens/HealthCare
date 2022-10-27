6e4c76e785a927661b1c51dc9f2fc89541cc5b64
MalekBens <bn.salah.malek@gmail.com>
2022-10-27 19:27:27 +0100

    Empty Project Structure

---
+++ lib/app/constants/theme/ColorsSchema.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+const Color orangeBtn = Color.fromARGB(255, 234, 185, 61);
+const Color greyText = Color.fromARGB(255, 178, 183, 201);
+const Color blackText = Color.fromARGB(255, 35, 35, 45);
+const Color navigationColor = Color.fromARGB(255, 122, 135, 251);
+const Color shadowColor = Color.fromARGB(255, 91, 115, 215);
+const Color selectedDayColor = Color.fromARGB(255, 243, 168, 163);
